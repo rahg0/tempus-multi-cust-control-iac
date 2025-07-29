6dc3078c9a9665017341d48af867f456b15bb63b
Raghu <raghu.esh@gmail.com>
2025-07-29 18:44:21 +0530

    Create test-file-22.tf

---
+++ test-file-22.tf
@@ -0,0 +1,33 @@
+module "foo_bucket" {
+  source = "git@github.com:example/repo/gcp/storage/bucket"
+
+  project_context = module.workload_context.project_context
+  name            = "foo-owners"
+  versioning      = false
+  lifecycle_rules = []
+}
+
+
+module "foo_owners_bucket_perms_4" {
+  storage_bucket = module.foo_bucket.bucket_name
+  member         = "serviceAccount:foo-service@random-project.iam.gserviceaccount.com"
+  roles = [
+    "roles/owner",
+  ]
+}
+
+module "foo_service_account_bindings_5" {
+  project_context = module.workload_context.project_context
+  member          = "serviceAccount:foo-service@random-project.iam.gserviceaccount.com"
+  roles = [
+    "roles/editor",
+  ]
+}
+
+module "foo_owners_bucket_perms_6" {
+  storage_bucket = module.foo_bucket.bucket_name
+  member         = "group:foo-group@random.com"
+  roles = [
+    "roles/storage.admin",
+  ]
+}
